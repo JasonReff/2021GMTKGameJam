bf08cc0ecfc5ebceae8eb7bbee745e3b994564d5
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-06-12 11:31:18 -0400

    Three shooting

--- Assets/Scripts/Enemies/ThreeEnemy.cs
+++ Assets/Scripts/Enemies/ThreeEnemy.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class ThreeEnemy : Enemy
 {
 
     public override void EnemyFire()
     {
-        Vector2 projectileOffset = enemyToPlayer.Perpendicular1();
-        GameObject projectile1 = Instantiate(enemyProjectile, gameObject.transform.position + projectileOffset);
-        GameObject projectile2 = Instantiate(enemyProjectile);
-        GameObject projectile3 = Instantiate(enemyProjectile);
-        projectile1.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer * projectile.GetComponent<Projectile>().projectileForce);
-        projectile2.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer.Perpendicular1() * projectile.GetComponent<Projectile>().projectileForce);
-        projectile3.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer.Perpendicular1() * projectile.GetComponent<Projectile>().projectileForce);
+        Vector2 newDirection = -enemyToPlayer / 10;
+        GameObject projectile1 = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection, Quaternion.identity);
+        GameObject projectile2 = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection + (Vector3)newDirection.Perpendicular1(), Quaternion.identity);
+        GameObject projectile3 = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection + (Vector3)newDirection.Perpendicular2(), Quaternion.identity);
+        Vector2 projectile2Direction = newDirection + (Vector2)newDirection.Perpendicular1();
+        Vector2 projectile3Direction = newDirection + (Vector2)newDirection.Perpendicular1();
+        projectile1.GetComponent<Rigidbody2D>().AddForce(newDirection * 10 * projectile1.GetComponent<Projectile>().projectileForce);
+        projectile2.GetComponent<Rigidbody2D>().AddForce(projectile2Direction * 10 * projectile2.GetComponent<Projectile>().projectileForce);
+        projectile3.GetComponent<Rigidbody2D>().AddForce(projectile3Direction * 10 * projectile3.GetComponent<Projectile>().projectileForce);
     }
 }
