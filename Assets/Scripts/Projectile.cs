876610532e80a39ee36412ed6407872602b147a8
JasonReff <jmr8382@rit.edu>
2021-06-12 09:51:51 -0400

    try to make projectiles ignore collisions

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,6 +7,15 @@ public class Projectile : MonoBehaviour
 
     public float projectileForce;
 
+    private void Awake()
+    {
+        Physics.IgnoreLayerCollision(8, 9);
+        Physics.IgnoreLayerCollision(8, 8);
+        Physics.IgnoreLayerCollision(9, 9);
+        Physics.IgnoreLayerCollision(6, 8);
+        Physics.IgnoreLayerCollision(7, 9);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
