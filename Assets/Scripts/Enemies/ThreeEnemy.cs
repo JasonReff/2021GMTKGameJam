5ff4d8ff1ae82b4281b846806ac92deb0ad66633
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-06-12 10:38:42 -0400

    nothing

--- Assets/Scripts/Enemies/ThreeEnemy.cs
+++ Assets/Scripts/Enemies/ThreeEnemy.cs
@@ -7,6 +7,12 @@ public class ThreeEnemy : Enemy
 
     public override void EnemyFire()
     {
-        //must make this
+        Vector2 projectileOffset = enemyToPlayer.Perpendicular1();
+        GameObject projectile1 = Instantiate(enemyProjectile, gameObject.transform.position + projectileOffset);
+        GameObject projectile2 = Instantiate(enemyProjectile);
+        GameObject projectile3 = Instantiate(enemyProjectile);
+        projectile1.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer * projectile.GetComponent<Projectile>().projectileForce);
+        projectile2.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer.Perpendicular1() * projectile.GetComponent<Projectile>().projectileForce);
+        projectile3.GetComponent<Rigidbody2D>().AddForce(enemyToPlayer.Perpendicular1() * projectile.GetComponent<Projectile>().projectileForce);
     }
 }
