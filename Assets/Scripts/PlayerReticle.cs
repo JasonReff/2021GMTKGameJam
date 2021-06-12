c46d49c5c8983835e0e8d222e1bb7e18f4400372
JasonReff <jmr8382@rit.edu>
2021-06-11 23:38:08 -0400

    move fire method and reticle object to player character script

--- Assets/Scripts/PlayerReticle.cs
+++ Assets/Scripts/PlayerReticle.cs
@@ -7,15 +7,11 @@ public class PlayerReticle : MonoBehaviour
     public PlayerCharacter character;
     public Vector2 characterToReticle;
     public Rigidbody2D rb;
-    public GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Fire();
-        }
+        
     }
 
     private void FixedUpdate()
@@ -23,13 +19,6 @@ public class PlayerReticle : MonoBehaviour
         rb.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 8);
         characterToReticle = rb.transform.position - character.rb.transform.position;
         characterToReticle.Normalize();
-
     }
 
-    public void Fire()
-    {
-        GameObject projectile = Instantiate(projectilePrefab);
-        projectile.transform.position = (Vector2)character.rb.transform.position + characterToReticle;
-        projectile.GetComponent<Rigidbody2D>().AddForce(characterToReticle * projectile.GetComponent<Projectile>().projectileForce);
-    }
 }
