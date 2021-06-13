547b551efaf1bdb975289246878920ed611cd6d4
JasonReff <jmr8382@rit.edu>
2021-06-12 23:48:09 -0400

    bracket projectile tilting

--- Assets/Scripts/Enemies/BracketEnemy.cs
+++ Assets/Scripts/Enemies/BracketEnemy.cs
@@ -8,7 +8,9 @@ public class BracketEnemy : Enemy
     public override void EnemyFire()
     {
         Vector2 newDirection = -enemyToPlayer / 10;
+        float rotation = Vector2.Angle(newDirection, new Vector2(-1, 0));
         GameObject projectile = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection, Quaternion.identity);
+        projectile.transform.Rotate(0, 0, rotation);
         projectile.GetComponent<Rigidbody2D>().AddForce(newDirection * 10 * projectile.GetComponent<Projectile>().projectileForce);
     }
 }
