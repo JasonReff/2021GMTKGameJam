b3f42d65b778d9407ac53e268097ca00a95cd03d
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-06-13 00:09:23 -0400

    Retry and scores

--- Assets/Scripts/ShowRoundAndScore.cs
+++ Assets/Scripts/ShowRoundAndScore.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowRoundAndScore : MonoBehaviour
 {
+    public Text roundTextbox;
+    public Text scoreTextbox;
     // Start is called before the first frame update
     void Start()
     {
-        roundTextbox.text = "Round: " + PlayerPrefs.GetInt("round", eSpawn.GetComponent<EnemySpawner>().round).ToString();
-        PlayerPrefs.GetInt("score", eSpawn.GetComponent<EnemySpawner>().score);
+        roundTextbox.text = "Round: " + PlayerPrefs.GetInt("round").ToString();
+        scoreTextbox.text = "Score: " + PlayerPrefs.GetInt("score").ToString();
     }
 
     // Update is called once per frame
