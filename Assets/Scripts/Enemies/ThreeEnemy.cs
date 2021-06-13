0b5eae8a4691ccccc92bf88c4286e89c9679c081
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-06-12 21:55:21 -0400

    WorkingThree

--- Assets/Scripts/Enemies/ThreeEnemy.cs
+++ Assets/Scripts/Enemies/ThreeEnemy.cs
@@ -12,7 +12,7 @@ public class ThreeEnemy : Enemy
         GameObject projectile2 = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection + new Vector3(newDirection.y, -newDirection.x), Quaternion.identity);
         GameObject projectile3 = Instantiate(enemyProjectile, gameObject.transform.position + (Vector3)newDirection + new Vector3(-newDirection.y, newDirection.x), Quaternion.identity);
         Vector2 projectile2Direction = newDirection + new Vector2(newDirection.y, -newDirection.x);
-        Vector2 projectile3Direction = newDirection + new Vector2(newDirection.y, -newDirection.x);
+        Vector2 projectile3Direction = newDirection + new Vector2(-newDirection.y, newDirection.x);
         projectile1.GetComponent<Rigidbody2D>().AddForce(newDirection * 10 * projectile1.GetComponent<Projectile>().projectileForce);
         projectile2.GetComponent<Rigidbody2D>().AddForce(projectile2Direction * 10 * projectile2.GetComponent<Projectile>().projectileForce);
         projectile3.GetComponent<Rigidbody2D>().AddForce(projectile3Direction * 10 * projectile3.GetComponent<Projectile>().projectileForce);
