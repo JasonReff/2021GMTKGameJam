3676d9d8a203458d3018bc905af7cdc6f2b69102
JasonReff <jmr8382@rit.edu>
2021-06-14 19:42:09 -0400

    import textmesh pro. import robot font as text mesh pro font

---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrade : MonoBehaviour
+{
+
+    public TextMeshProUGUI upgradeName;
+    public TextMeshProUGUI upgradeDescription;
+    public int cost;
+
+    public virtual void UpgradeEffect()
+    {
+
+    }
+
+}
